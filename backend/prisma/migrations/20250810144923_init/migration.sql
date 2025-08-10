86f41716351637719e78992fe9b1d91780787d50
Nunisabivek <bivek.nunisa.adtu@gmail.com>
2025-08-10 20:19:42 +0530

    init postgres migrations for Supabase

---
+++ backend/prisma/migrations/20250810144923_init/migration.sql
@@ -0,0 +1,26 @@
+-- CreateTable
+CREATE TABLE "public"."User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT,
+    "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "plan" TEXT NOT NULL DEFAULT 'FREE',
+    "credits" INTEGER NOT NULL DEFAULT 5,
+    "subscriptionId" TEXT,
+    "referralCode" TEXT,
+    "referredByCode" TEXT,
+    "referralDiscountEligible" BOOLEAN NOT NULL DEFAULT false,
+    "referralDiscountUsed" BOOLEAN NOT NULL DEFAULT false,
+    "resetToken" TEXT,
+    "resetTokenExpiry" TIMESTAMP(3),
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "public"."User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_referralCode_key" ON "public"."User"("referralCode");
